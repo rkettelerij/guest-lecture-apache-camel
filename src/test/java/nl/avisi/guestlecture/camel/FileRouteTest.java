0768c43156e17da6745e34acdc329e865563ac25
Richard Kettelerij <r.kettelerij@avisi.nl>
2014-01-06 11:57:43 +0100

    Removed AdviceBuilder, since isMockEndpointsAndSkip is probably simpler to understand.

--- src/test/java/nl/avisi/guestlecture/camel/FileRouteTest.java
+++ src/test/java/nl/avisi/guestlecture/camel/FileRouteTest.java
@@ -1,23 +1,18 @@
 package nl.avisi.guestlecture.camel;
 
-import nl.avisi.guestlecture.camel.FileRoute;
-
-import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 public class FileRouteTest extends CamelTestSupport {
 
+    @Override
+    public String isMockEndpointsAndSkip() {
+        return "file:outbox*";
+    }
+
     @Test
     public void testOutboxContainsFile() throws Exception {
-        context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                mockEndpointsAndSkip("file:outbox/");
-            }
-        });
-
         getMockEndpoint("mock:file:outbox/").expectedMessageCount(1);
         getMockEndpoint("mock:file:outbox/").expectedBodiesReceived("Goodbye World");
 
@@ -28,13 +23,6 @@ public class FileRouteTest extends CamelTestSupport {
 
     @Test
     public void testOutboxKlaasContainsFile() throws Exception {
-        context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                mockEndpointsAndSkip("file:outbox/", "file:outbox-klaas/");
-            }
-        });
-
         getMockEndpoint("mock:file:outbox/").expectedMessageCount(2); // aap + noot
         getMockEndpoint("mock:file:outbox-klaas/").expectedMessageCount(1); // klaas
 
